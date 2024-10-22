65a1740aa6f1130616f532c419839184bf13625d
aslam <thoppilaslam0987654321@gmail.com>
2024-10-22 19:46:30 +0530

    new

--- project/app/views.py
+++ project/app/views.py
@@ -26,5 +26,39 @@ def task2(req,num):
         return HttpResponse('divisible by 3')
     else:
         return HttpResponse('not divisible by 3')
+    
+def task3(req,day):
+    if day==1:
+        return HttpResponse('sunday')
+    elif day==2:
+        return HttpResponse('monday')
+    elif day==3:
+        return HttpResponse('tuesday')
+    elif day==4:
+        return HttpResponse('wednesday')
+    elif day==5:
+        return HttpResponse('thursday')
+    elif day==6:
+        return HttpResponse('friday')
+    elif day==7:
+        return HttpResponse('saturday')
+    else:
+        return HttpResponse('invalid')
+
+def task4(req,cost):
+    if cost>100000:
+        return HttpResponse(cost*0.15)
+    elif cost>50000 and cost<=100000:
+        return HttpResponse(cost*0.10)
+    else:
+        return HttpResponse(cost*0.05)
+
+def task5(req,unit):
+    if unit<=100:
+        return HttpResponse(unit*0)
+    elif unit>100 and unit<=200:
+        return HttpResponse((unit-100)*5)
+    else:
+        return HttpResponse((unit-200)*10+500)
 
         # Create your views here.
--- project/project/urls.py
+++ project/project/urls.py
@@ -24,6 +24,10 @@ urlpatterns = [
     path('fun1/<a>',views.fun1),
     path('task/<int:salary>/<int:year>',views.task),
     path('task1/<city>',views.task1),
-    path('task2/<int:num>',views.task2)
+    path('task2/<int:num>',views.task2),
+    path('task3/<day>',views.task3),
+    path('task4/<int:cost>',views.task4),
+    path('task5/<int:unit>',views.task5),
+
 
 ]
