0d6ec3533e996944eabdf3d0dc9b5a640e739e9f
aslam <thoppilaslam0987654321@gmail.com>
2024-11-12 12:06:07 +0530

    new commit

--- drs_shop/app/views.py
+++ drs_shop/app/views.py
@@ -1,3 +1,84 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from .models import *
+import os
+from django.contrib.auth.models import *
+from django.contrib import messages
+
 
 # Create your views here.
+
+def drs_login(req):
+    if 'dress' in req.session:
+        return redirect(home)
+    if req.method=='POST':
+        uname=req.POST['uname']
+        password=req.POST['passwd']
+        data=authenticate(username=uname,password=password)
+        if data:
+            login(req,data)
+            req.session['dress']=uname   #create session
+            return redirect(home)
+        else:
+            messages.warning(req,'Invalid username or password.')
+            return redirect(drs_login)
+    else:
+        return render(req,'login.html')
+    
+def home(req):
+    if 'dress' in req.session:
+        data=Product.objects.all()
+        return render(req,'shop/home.html',{'data':data})
+    else:
+        return redirect(drs_login)
+    
+def drs_logout(req):
+    req.session.flush()          #delete session
+    logout(req)
+    return redirect(drs_login)
+
+def add_prod(req):
+    if 'dress' in req.session:
+        if req.method=='POST':
+            prd_id=req.POST['prd_id']
+            prd_name=req.POST['prd_name']
+            prd_price=req.POST['prd_price']
+            ofr_price=req.POST['ofr_price']
+            img=req.FILES['img']
+            data=Product.objects.create(pro_id=prd_id,name=prd_name,price=prd_price,offer_price=ofr_price,img=img)
+            data.save()
+            return redirect(add_prod)
+        else:
+            return render(req,'shop/add_prod.html')
+    else:
+        return redirect(drs_login)
+    
+def edit(req,pid):
+    if 'dress' in req.session:
+        if req.method=='POST':
+            prd_id=req.POST['prd_id']
+            prd_name=req.POST['prd_name']
+            prd_price=req.POST['prd_price']
+            ofr_price=req.POST['ofr_price']
+            img=req.FILES.get('img')
+            if img:
+                Product.objects.filter(pk=pid).update(pro_id=prd_id,name=prd_name,price=prd_price,offer_price=ofr_price)
+                data=Product.objects.get(pk=pid)
+                data.img=img
+                data.save()
+            else:
+                Product.objects.filter(pk=pid).update(pro_id=prd_id,name=prd_name,price=prd_price,offer_price=ofr_price)
+            return redirect(home)
+        else:
+            data=Product.objects.get(pk=pid)
+            return render(req,'shop/edit.html',{'product':data})
+    else:
+        return redirect(drs_login)
+
+def delete(req,pid):
+    data=Product.objects.get(pk=pid)
+    url=data.img.url
+    og_path=url.split('/')[-1]
+    os.remove('media/'+og_path)
+    data.delete()
+    return redirect(home)
